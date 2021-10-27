1d64fc286bcdc8bac3e8134c760ad1b18c945b0f
John Michael Martinez Tenorio <jWX970139@china.huawei.com>
2021-10-27 00:47:07 -0500

    def ga

--- genetic_scheduler.py
+++ genetic_scheduler.py
@@ -1,7 +1,25 @@
 #import
 import os
+import numpy as np
+from deap import algorithms, base, creator, tools
+
+def calculate_peso(rbs, modulacion):
+	return rbs*modulacion
+
+def main():
+	'''main'''
+	rbs=[10, 10, 20, 30]
+	modulacion=[2, 4, 1, 3]
+	pj=calculate_peso(rbs, modulacion)
+	W=sum(rbs)
+
+	#algoritmo genetico (algoritmo cuantico)
+
+
+
+
 if __name__=="__main__":
 	#Prototipo:
-	pass
+	main()
 else:
 	print("Modulo Importado: [", os.path.basename(__file__), "]")
